c178304dd649a905036eb0a797ec6bc85b8f5ed8
Patrik Olofsson <Patrik.d.olofsson@gmail.com>
2015-10-26 17:34:20 +0100

    Skapar test,textures package.

---
+++ GroupProject/src/test/Main.java
@@ -0,0 +1,61 @@
+package test;
+	
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application 
+{
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		Group root = new Group();
+		Scene theScene = new Scene(root);
+		
+		primaryStage.setScene(theScene);
+		
+		Canvas canvas = new Canvas(500,500);
+		root.getChildren().add(canvas);
+		
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+
+		Image sun = new Image("textures/sun.png");
+		Image space = new Image("textures/space.jpg");
+
+
+
+
+		final long startNanoTime = System.nanoTime();
+		 
+	    new AnimationTimer()
+	    {
+	        public void handle(long currentNanoTime)
+	        {
+	            double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
+	 
+	            double x1 =  -500 * Math.cos(t);
+	            double y1 = 1; // Math.sin(t);
+	    
+	            // background image clears canvas
+	            gc.drawImage( space,x1,y1);
+
+	            gc.drawImage( sun, 100, 70 );
+	        }
+	    }.start();
+	 
+
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+}
