d289714c7d065166fafeadd381d1a7b00f76bd00
Valmorbus <borgstrom.simon@gmail.com>
2015-10-26 15:36:45 +0100

    Added basis for main menu.

--- GroupProject/src/groupProject/MainWindow.java
+++ GroupProject/src/groupProject/MainWindow.java
@@ -1,12 +1,35 @@
 package groupProject;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		
+		
+		// Princip för startfönster som tar en vidare till spelet
+		Button n = new Button("New GAme");
+		Group root = new Group();
+		Scene scene = new Scene(root);
+		root.getChildren().add(n);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		Group g = new Group();
+		Label label = new Label("hej");
+		g.getChildren().add(label);
+		Scene scene2 = new Scene(g);
+		
+		n.setOnAction(event ->{
+			primaryStage.setScene(scene2);
+		});
+		
 		
 	}
 
