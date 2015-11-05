3191bccdfd0ceebb736f9656aa77a038e12e120b
PatrikHornqvist <patrik.hornqvist@yh.nackademin.se>
2015-11-05 11:28:03 +0100

    JUnit klass tillagd. Ej fullt implementerad.

---
+++ GroupProject/src/ninjagametest/HighscoreTest.java
@@ -0,0 +1,29 @@
+package ninjagametest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HighscoreTest {
+
+	@Test
+	public void testSortList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+}
