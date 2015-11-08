f2c443bfb55044b23e171463d7667932b9438225
PatrikHornqvist <patrik.hornqvist@yh.nackademin.se>
2015-11-08 21:14:01 +0100

    Städade upp i JUnit-klassen.

--- GroupProject/src/ninjagame/HighscoreTest.java
+++ GroupProject/src/ninjagame/HighscoreTest.java
@@ -8,25 +8,12 @@ import org.junit.Test;
 
 public class HighscoreTest {
 	
-
-	/*
-	@Test // Annotation som visar att detta är ett JUnit test.
-	public void testSortList() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testWriteFile() {
-		fail("Not yet implemented");
-	}
-	*/
-	
 	@Test
 	public void readFileShouldReurnLinkedList() {
 		assertThat(Highscore.readFile(), instanceOf(LinkedList.class));
 	}
 
-	@Test(timeout=10) // Annotation som visar att det är ett JUnit test.	
+	@Test(timeout=10)	
 	public void readFileShouldBeFast(){
 		
 		Highscore.readFile();
